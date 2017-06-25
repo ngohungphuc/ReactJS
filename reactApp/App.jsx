1bf8776c61692e73af977b6bde6b12d1cd36d282
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-06-25 11:57:43 +0700

    First app

---
+++ reactApp/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello Tony !!!
+            </div>
+        );
+    }
+}
+
+export default App;
