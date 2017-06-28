dc497c3762ec1bce0c34a2876ae32fb2d0ab8eed
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-06-28 23:19:14 +0700

    Taking Input from the User

--- ReactJs-Getting-Start/github-api-card/src/App.js
+++ ReactJs-Getting-Start/github-api-card/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let axios = require('axios');
+
 const Card = (props) => {
   return (
     <div style={{
@@ -24,9 +26,62 @@ const CardList = (props) => {
     <div>
       {props
         .cards
-        .map(card => <Card {...card}/>)}
+        .map(card => <Card key={card.id} {...card}/>)}
     </div>
   );
 }
 
-export default CardList;
+class Form extends React.Component {
+  state = {
+    userName: ''
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userName}`)
+      .then(resp => {
+        this
+          .props
+          .onSubmit(resp.data);
+        this.setState({userName: ''});
+      });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.userName}
+          onChange={(event) => this.setState({userName: event.target.value})}
+          placeholder="Github username"
+          required/>
+        <button type="submit">Add card</button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    cards: []
+  }
+
+  addNewCard = (cardInfo) => {
+    this.setState(prevState => ({
+      cards: prevState
+        .cards
+        .concat(cardInfo)
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.addNewCard}/>
+        <CardList cards={this.state.cards}/>
+      </div>
+    );
+  }
+}
+export default App;
--- ReactJs-Getting-Start/github-api-card/src/index.js
+++ ReactJs-Getting-Start/github-api-card/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CardList from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-let data = [
-  {
-    name: "Tony Hudson",
-    avatar_url: "https://avatars0.githubusercontent.com/u/13591213?v=3",
-    company: "Nash Tech"
-  }
-]
 
-ReactDOM.render(<CardList cards={data}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
