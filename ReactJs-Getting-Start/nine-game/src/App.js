7c0dbea0fd168d56819c84e53e468969c4d91bbd
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-07-01 16:30:17 +0700

    The State of Selected Numbers

--- ReactJs-Getting-Start/nine-game/src/App.js
+++ ReactJs-Getting-Start/nine-game/src/App.js
@@ -29,24 +29,37 @@ const Button = (props) => {
 const Answer = (props) => {
   return (
     <div className="col-md-5">
-      <span>5</span>
+      {props
+        .selectedNumbers
+        .map((number, i) => <span key={i}>{number}</span>)}
     </div>
   )
 }
 
 const Numbers = (props) => {
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  }
   return (
     <div className="card text-center">
       <div>
-        {Numbers.list.map((number, i) => <span key={i}>{number}</span>)}
+        {Numbers
+          .list
+          .map((number, i) => <span key={i} className={numberClassName(number)}>{number}</span>)}
       </div>
     </div>
   )
 }
 
-Numbers.list = _.range(1,10);
+Numbers.list = _.range(1, 10);
 
 class Game extends Component {
+  state = {
+    selectedNumbers: [2, 4]
+  };
+
   render() {
     return (
       <div>
@@ -54,10 +67,10 @@ class Game extends Component {
         <div className="row">
           <Stars/>
           <Button/>
-          <Answer/>
+          <Answer selectedNumbers={this.state.selectedNumbers}/>
         </div>
         <br/>
-        <Numbers/>
+        <Numbers selectedNumbers={this.state.selectedNumbers}/>
       </div>
     );
   }
