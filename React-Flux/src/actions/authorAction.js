055bf39698640a1ef6bfdf973a7025877a6d148b
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2017-07-11 23:11:26 +0700

    action creater

---
+++ React-Flux/src/actions/authorAction.js
@@ -0,0 +1,23 @@
+/*
+ * @CreateTime: Jul 11, 2017 10:44 PM
+ * @Author: Tony Hudson
+ * @Contact: github.com/ngohungphuc
+ * @Last Modified By: Tony Hudson
+ * @Last Modified Time: Jul 11, 2017 11:10 PM
+ * @Description: Modify Here, Please
+ */
+
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionType');
+var AuthorActions = {
+    createAuthor: function (author) {
+        var newAuthor = AuthorApi.saveAuthor(author);
+        //tell all the stores that an author was just created
+        Dispatcher.dispatch({actionType: ActionTypes.CREATE_AUTHOR, atuhor: newAuthor})
+    }
+};
+
+module.exports = AuthorActions;
---
+++ React-Flux/src/constants/actionType.js
@@ -0,0 +1,14 @@
+/*
+ * @CreateTime: Jul 11, 2017 11:06 PM
+ * @Author: Tony Hudson
+ * @Contact: github.com/ngohungphuc
+ * @Last Modified By: Tony Hudson
+ * @Last Modified Time: Jul 11, 2017 11:07 PM
+ * @Description: Modify Here, Please
+ */
+
+"use strict";
+
+var keyMirror = require('react/lib/keyMirror');
+
+module.exports = keyMirror({CREATE_AUTHOR: null});
