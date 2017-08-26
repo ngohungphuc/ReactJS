e0202ea23e16da2874aaaedaa89df69aaa725306
ngohungphuc <ngohungphuc95@gmail.com>
2017-08-26 20:16:00 +0700

    second routes

--- FullStack-React/server/routes.js
+++ FullStack-React/server/routes.js
@@ -1,3 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost/local");
+
+const articleSchema = {
+  articleTitle: String,
+  articleContent: String
+};
+
+const Article = mongoose.model("Article", articleSchema, "articles");
+
 const PublishingAppRoutes = [
   {
     route: "articles.length",
@@ -7,6 +18,31 @@ const PublishingAppRoutes = [
         path: ["articles", "length"],
         value: articlesCountInDb
       };
+    }
+  },
+  {
+    route: 'articles[{intergers}]["id","articleTitle","articleContent"]',
+    get: pathSet => {
+      const articlesIndex = pathSet[1];
+
+      return Article.find(
+        {},
+        (err, articlesDocs) => articlesDocs
+      ).then(articlesArrayFromDb => {
+        let results = [];
+
+        articlesIndex.forEach(index => {
+          const singleArticleObject = articlesArrayFromDb[index].toObject();
+          const falcorSingleArticleResult = {
+            path: ["articles", index],
+            value: singleArticleObject
+          };
+
+          results.push(falcorSingleArticleResult);
+        });
+
+        return results;
+      });
     }
   }
 ];
