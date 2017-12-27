bba8f8019510ff1fa3d1239703266115418c0552
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-27 17:55:07 +0700

    dynamic component

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-import Aux from '../../hoc/Auxi';
+import Aux from "../../hoc/Auxi";
 
 class BurgerBuilder extends Component {
-    render() {
-        return (
-            <Aux>
-            <div>Burger </div>
-            <div>Build controls</div>
-            </Aux>
-        );
-    }
+  render() {
+    return (
+      <Aux>
+        <div>Burger </div>
+        <div>Build controls</div>
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
