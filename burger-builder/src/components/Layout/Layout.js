52e9b6527c408eb6872101d0184aef7491a00d72
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-27 17:42:07 +0700

    update code

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Auxi";
 import classes from './Layout.css';
 
 const layout = props => (
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxi';
 
 class BurgerBuilder extends Component {
     render() {
