1de74c3cb51ec737632ccaaea17b88b7a8a248ec
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-28 19:33:14 +0700

    updated price

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,6 +65,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disableInfo}
+          price={this.state.totalPrice}
           />
       </Aux>
     );
