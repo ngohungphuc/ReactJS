2cd6711f9bd1c3b527fd7586f7a7da6ad98aebc4
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-28 21:43:18 +0700

    backdrop components

---
+++ burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Backdrop.css";
+
+const backdrop = props =>
+  props.show ? <div className={classes.Backdrop} onClick={props.clicked} /> : null;
+
+export default backdrop;
