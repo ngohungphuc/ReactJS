c5a4e185f9babb199bf46d8cfa8a40a0c186c99c
ngohungphuc <ngohungphuc95@gmail.com>
2017-12-28 20:52:58 +0700

    order summary modal

---
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Aux from "../../../hoc/Auxi";
+
+const orderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+    return (
+      <li key={igKey}>
+        {igKey} : {props.ingredients[igKey]}
+      </li>
+    );
+  });
+  return (
+    <Aux>
+      <h3>Your order</h3>
+      <p>Following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout</p>
+    </Aux>
+  );
+};
+
+export default orderSummary;
