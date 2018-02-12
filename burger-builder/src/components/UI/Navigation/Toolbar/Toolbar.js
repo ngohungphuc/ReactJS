986aace0fdb3733e06b8ee357b087b9ebbf32c37
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-12 21:38:43 +0700

    add nav item

--- burger-builder/src/components/UI/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/UI/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import classes from "./Toolbar.css";
 import Logo from "../../../Logo/Logo";
+import NavigationItems from "./../NavigationItems/NavigationItems";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
@@ -9,7 +10,9 @@ const toolbar = props => (
     <div className={classes.Logo}>
       <Logo />
     </div>
-    <nav className={classes.DesktopOnly} />
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
