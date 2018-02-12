bd765cdd43a83ed18195df718af2b9fa48df8912
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-12 20:47:43 +0700

    add price

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,6 +14,9 @@ const orderSummary = props => {
       <h3>Your order</h3>
       <p>Following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to checkout</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>
         CANCEL
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,6 +95,7 @@ class BurgerBuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinue={this.purchaseContinueHandler}
           />
