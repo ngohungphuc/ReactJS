73c7f81c8a4bb930f81aa751ebaa89c717038618
Tony Hudson <ngohungphuc95@gmail.com>
2018-02-13 22:44:11 +0700

    fetch dat on update

--- http/src/containers/Blog/Blog.js
+++ http/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from "axios";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -23,15 +24,27 @@ class Blog extends Component {
       this.setState({ posts: updatedPosts });
     });
   }
+
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          clicked={() => this.postSelectedHandler(post.id)}
+          key={post.id}
+          title={post.title}
+          author={post.author}
+        />
+      );
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
